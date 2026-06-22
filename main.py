4b92e90b5cc87e5e34e365c34d60b7dd20d78c03
fedalegend <fparhomenko79@gmail.com>
2026-06-22 19:18:33 +0300

    анотация типов в функции output

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ def get_user_name() -> str | None:
     input_string = input().strip()
     return input_string if input_string else None
 
-def output_welcome(user_name):
+def output_welcome(user_name: str | None) -> None:
     print(f"hello {user_name}!" if user_name else f"hello nameless!")
 
 output_welcome(get_user_name())
